4bea22add645425252359fbf2938ab30bc8962f4
ChinmayLonkar <chinmay20220@gmail.com>
2021-10-22 00:04:35 +0530

    Added i2c c file

--- main/main.c
+++ main/main.c
@@ -20,25 +20,72 @@
 #include "driver/i2s.h"
 
 #include "tuning_http_server.h"
+#include "driver/i2c.h"
 
-#include "sra_board.h"
+#define I2C_MASTER_SCL_IO           22                         /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           21                         /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
 
-// static xTaskHandle start_bluetooth_hdl = NULL;
+#define WM8960_AUDIO_BOARD_ADDR                 0x34       /*!< Slave address of WM8960 Audio Board */
+#define R0   0x01
+#define R1   0x03
+#define R2   0x05
+#define R3   0x07
+#define R5   0x0A
+#define R26  0x35
+#define R34  0x45
+#define R37  0x4B
+#define R0_DATA  0x17
+#define R1_DATA  0x17
+#define R2_DATA  0xFD
+#define R3_DATA  0xFD
+#define R5_DATA  0x00
+#define R26_DATA 0xE0
+#define R34_DATA 0x50
+#define R37_DATA 0x50
+static const char *TAG = "i2c-WM8960-CONFIG";
 
-// #define CONFIG_EXAMPLE_A2DP_SINK_OUTPUT_INTERNAL_DAC
 
-/* event for handler "bt_av_hdl_stack_up */
-// enum {
-//     BT_APP_EVT_STACK_UP = 0,
-// };
+static esp_err_t WM8960_AUDIO_BOARD_register_write_byte(uint8_t reg_addr, uint8_t data)
+{
+    int ret;
+    uint8_t write_buf[2] = {reg_addr, data};
 
-/* handler for bluetooth stack enabled events */
-// void bt_av_hdl_stack_evt(uint16_t event, void *p_param);
+    ret = i2c_master_write_to_device(I2C_MASTER_NUM, WM8960_AUDIO_BOARD_ADDR  , write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_RATE_MS);
+
+    return ret;
+}
+
+
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
 
 
 void app_main(void)
 {
 
+    ESP_ERROR_CHECK(i2c_master_init());
+    ESP_LOGI(TAG, "I2C initialized successfully");
 
       /* Initialize NVS — it is used to store PHY calibration data */
     esp_err_t err = nvs_flash_init();
@@ -47,6 +94,28 @@ void app_main(void)
         err = nvs_flash_init();
     }
     ESP_ERROR_CHECK(err);
+
+     /* Demonstrate writing by reseting the WM8960_AUDIO_BOARD */
+    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R0 , R0_DATA));
+    
+    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R1 , R1_DATA));
+  
+    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R2 , R2_DATA));
+
+    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R3 , R3_DATA));
+
+    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R5 , R5_DATA));
+ 
+    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R26 , R26_DATA));
+ 
+    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R34 , R34_DATA));
+    //vTaskDelay(50 / portTICK_PERIOD_MS);
+    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R37 , R37_DATA));
+   
+
+
+
+
 
     i2s_config_t i2s_config = {
 #ifdef CONFIG_EXAMPLE_A2DP_SINK_OUTPUT_INTERNAL_DAC
@@ -104,178 +173,6 @@ void app_main(void)
         return;
     }
     
-//     /* Initialize NVS — it is used to store PHY calibration data */
-//     esp_err_t err = nvs_flash_init();
-//     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-//         ESP_ERROR_CHECK(nvs_flash_erase());
-//         err = nvs_flash_init();
-//     }
-//     ESP_ERROR_CHECK(err);
-
-//     i2s_config_t i2s_config = {
-// #ifdef CONFIG_EXAMPLE_A2DP_SINK_OUTPUT_INTERNAL_DAC
-//         .mode = I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN,
-// #else
-//         .mode = I2S_MODE_MASTER | I2S_MODE_TX,                                  // Only TX
-// #endif
-//         .sample_rate = 44100,
-//         .bits_per_sample = 16,
-//         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           //2-channels
-//         .communication_format = I2S_COMM_FORMAT_STAND_MSB,
-//         .dma_buf_count = 6,
-//         .dma_buf_len = 60,
-//         .intr_alloc_flags = 0,                                                  //Default interrupt priority
-//         .tx_desc_auto_clear = true                                              //Auto clear tx descriptor on underflow
-//     };
-
-
-//     i2s_driver_install(0, &i2s_config, 0, NULL);
-// #ifdef CONFIG_EXAMPLE_A2DP_SINK_OUTPUT_INTERNAL_DAC
-//     i2s_set_dac_mode(I2S_DAC_CHANNEL_BOTH_EN);
-//     i2s_set_pin(0, NULL);
-// #else
-//     i2s_pin_config_t pin_config = {
-//         .bck_io_num = CONFIG_EXAMPLE_I2S_BCK_PIN,
-//         .ws_io_num = CONFIG_EXAMPLE_I2S_LRCK_PIN,
-//         .data_out_num = CONFIG_EXAMPLE_I2S_DATA_PIN,
-//         .data_in_num = -1                                                       //Not used
-//     };
-
-//     i2s_set_pin(0, &pin_config);
-// #endif
-
-
-//     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
-
-//     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-//     if ((err = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
-//         ESP_LOGE(BT_AV_TAG, "%s initialize controller failed: %s\n", __func__, esp_err_to_name(err));
-//         return;
-//     }
-
-//     if ((err = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
-//         ESP_LOGE(BT_AV_TAG, "%s enable controller failed: %s\n", __func__, esp_err_to_name(err));
-//         return;
-//     }
-
-//     if ((err = esp_bluedroid_init()) != ESP_OK) {
-//         ESP_LOGE(BT_AV_TAG, "%s initialize bluedroid failed: %s\n", __func__, esp_err_to_name(err));
-//         return;
-//     }
-
-//     if ((err = esp_bluedroid_enable()) != ESP_OK) {
-//         ESP_LOGE(BT_AV_TAG, "%s enable bluedroid failed: %s\n", __func__, esp_err_to_name(err));
-//         return;
-//     }
-
-//     /* create application task */
-//     bt_app_task_start_up();
-
-//     /* Bluetooth device name, connection mode and profile set up */
-//     bt_app_work_dispatch(bt_av_hdl_stack_evt, BT_APP_EVT_STACK_UP, NULL, 0, NULL);
-
-// #if (CONFIG_BT_SSP_ENABLED == true)
-//     /* Set default parameters for Secure Simple Pairing */
-//     esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
-//     esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_IO;
-//     esp_bt_gap_set_security_param(param_type, &iocap, sizeof(uint8_t));
-// #endif
-
-//     /*
-//      * Set default parameters for Legacy Pairing
-//      * Use fixed pin code
-//      */
-//     esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_FIXED;
-//     esp_bt_pin_code_t pin_code;
-//     pin_code[0] = '1';
-//     pin_code[1] = '2';
-//     pin_code[2] = '3';
-//     pin_code[3] = '4';
-//     esp_bt_gap_set_pin(pin_type, 4, pin_code);
-
-start_tuning_http_server();
-
-// xTaskCreate(start_bluetooth, "Bluetooth initializer", 4096, NULL, 1, &start_bluetooth_hdl);
-
-
+    start_tuning_http_server();
 }
 
-// void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
-// {
-//     switch (event) {
-//     case ESP_BT_GAP_AUTH_CMPL_EVT: {
-//         if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
-//             ESP_LOGI(BT_AV_TAG, "authentication success: %s", param->auth_cmpl.device_name);
-//             esp_log_buffer_hex(BT_AV_TAG, param->auth_cmpl.bda, ESP_BD_ADDR_LEN);
-//         } else {
-//             ESP_LOGE(BT_AV_TAG, "authentication failed, status:%d", param->auth_cmpl.stat);
-//         }
-//         break;
-//     }
-
-// #if (CONFIG_BT_SSP_ENABLED == true)
-//     case ESP_BT_GAP_CFM_REQ_EVT:
-//         ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_CFM_REQ_EVT Please compare the numeric value: %d", param->cfm_req.num_val);
-//         esp_bt_gap_ssp_confirm_reply(param->cfm_req.bda, true);
-//         break;
-//     case ESP_BT_GAP_KEY_NOTIF_EVT:
-//         ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_KEY_NOTIF_EVT passkey:%d", param->key_notif.passkey);
-//         break;
-//     case ESP_BT_GAP_KEY_REQ_EVT:
-//         ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_KEY_REQ_EVT Please enter passkey!");
-//         break;
-// #endif
-
-//     default: {
-//         ESP_LOGI(BT_AV_TAG, "event: %d", event);
-//         break;
-//     }
-//     }
-//     return;
-// }
-// void bt_av_hdl_stack_evt(uint16_t event, void *p_param)
-// {
-//     char *bt_name_by_user = read_bt_name().bt_name;
-//     ESP_LOGE(BT_AV_TAG, "BT_NAME_in main %s", bt_name_by_user);
-//     // if (strcmp(bt_name_by_user, "jn") != 0){
-//     //     esp_restart();
-//     // }
-//     // ESP_LOGE(BT_AV_TAG, "%s evt %d", __func__, bt_name_by_user);
-  
-//     ESP_LOGD(BT_AV_TAG, "%s evt %d", __func__, event);
-//     switch (event) {
-//     case BT_APP_EVT_STACK_UP: {
-//         /* set up device name */
-//         char *dev_name = bt_name_by_user;
-
-//         // char *dev_name = "helo";
-
-//         esp_bt_dev_set_device_name(dev_name);
-
-//         esp_bt_gap_register_callback(bt_app_gap_cb);
-
-//         /* initialize AVRCP controller */
-//         esp_avrc_ct_init();
-//         esp_avrc_ct_register_callback(bt_app_rc_ct_cb);
-//         /* initialize AVRCP target */
-//         assert (esp_avrc_tg_init() == ESP_OK);
-//         esp_avrc_tg_register_callback(bt_app_rc_tg_cb);
-                                                                   
-//         esp_avrc_rn_evt_cap_mask_t evt_set = {0};
-//         esp_avrc_rn_evt_bit_mask_operation(ESP_AVRC_BIT_MASK_OP_SET, &evt_set, ESP_AVRC_RN_VOLUME_CHANGE);
-//         assert(esp_avrc_tg_set_rn_evt_cap(&evt_set) == ESP_OK);
-
-//         /* initialize A2DP sink */
-//         esp_a2d_register_callback(&bt_app_a2d_cb);
-//         esp_a2d_sink_register_data_callback(bt_app_a2d_data_cb);
-//         esp_a2d_sink_init();
-
-//         /* set discoverable and connectable mode, wait to be connected */
-//         esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
-//         break;
-//     }
-//     default:
-//         ESP_LOGE(BT_AV_TAG, "%s unhandled evt %d", __func__, event);
-//         break;
-//     }
-// }
