0dd6a583d3f5ead3463e5e8b05fb096bde4099c5
ChinmayLonkar <chinmay20220@gmail.com>
2021-10-22 22:34:32 +0530

    updated main.c

---
+++ components/WM8960/WM8960.h
@@ -0,0 +1,23 @@
+
+#ifndef _WM8960_H_
+#define _WM8960_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <driver/i2c.h>
+#include <driver/i2s.h>
+#include "esp_err.h"
+#include "esp_log.h"
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
--- main/main.c
+++ main/main.c
@@ -8,7 +8,7 @@
 #include "nvs_flash.h"
 #include "esp_system.h"
 #include "esp_log.h"
-
+#include "esp_err.h"
 #include "esp_bt.h"
 #include "bt_app_core.h"
 #include "bt_app_av.h"
@@ -22,96 +22,232 @@
 #include "tuning_http_server.h"
 #include "driver/i2c.h"
 
-#define I2C_MASTER_SCL_IO           22                         /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           21                         /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
-#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS       1000
+static const char *TAG = "WM8960";
 
-#define WM8960_AUDIO_BOARD_ADDR                 0x34       /*!< Slave address of WM8960 Audio Board */
-#define R0   0x01
-#define R1   0x03
-#define R2   0x05
-#define R3   0x07
-#define R5   0x0A
-#define R26  0x35
-#define R34  0x45
-#define R37  0x4B
-#define R0_DATA  0x17
-#define R1_DATA  0x17
-#define R2_DATA  0xFD
-#define R3_DATA  0xFD
-#define R5_DATA  0x00
-#define R26_DATA 0xE0
-#define R34_DATA 0x50
-#define R37_DATA 0x50
-static const char *TAG = "i2c-WM8960-CONFIG";
+static uint8_t volume;
+#define CODEC_ADDR  0x34
+#define SDA_PIN  21
+#define SCL_PIN  22
+#define I2C_BUS_NO 0
+#define ACK_CHECK_EN  0
+#define ACK_CHECK_DIS 0
+#define ACK_VAL       0
+#define NACK_VAL      1
+const uint16_t wm8960_reg_defaults[] = {
+  0x0, 0x0117,
+  0x1, 0x0117,
+  0x2, 0x01fd,
+  0x3, 0x01fd,
+  0x4, 0x0000,
+  0x5, 0x0000,
+  0x6, 0x0000,
+  0x7, 0x000A,
+  0x8, 0x01c0,
+  0x9, 0x0000,
+  0xa, 0x01ff,
+  0xb, 0x01ff,
+
+ 0x10, 0x0000,
+ 0x11, 0x007b,
+ 0x12, 0x0100,
+ 0x13, 0x0032,
+ 0x14, 0x0000,
+ 0x15, 0x00c3,
+ 0x16, 0x00c3,
+ 0x17, 0x01c0,
+ 0x18, 0x0000,
+ 0x19, 0x0000,
+ 0x1a, 0x01e0,
+ 0x1b, 0x0000,
+ 0x1c, 0x0000,
+ 0x1d, 0x0000,
+
+ 0x20, 0x0100,
+ 0x21, 0x0100,
+ 0x22, 0x0150,
+
+ 0x25, 0x0150,
+ 0x26, 0x0000,
+ 0x27, 0x0000,
+ 0x28, 0x0000,
+ 0x29, 0x0000,
+ 0x2a, 0x0040,
+ 0x2b, 0x0000,
+ 0x2c, 0x0000,
+ 0x2d, 0x0050,
+ 0x2e, 0x0050,
+ 0x2f, 0x0000,
+ 0x30, 0x0002,
+ 0x31, 0x0037,
+
+ 0x33, 0x0080,
+ 0x34, 0x0008,
+ 0x35, 0x0031,
+ 0x36, 0x0026,
+ 0x37, 0x00e9,
+
+};
 
 
-static esp_err_t WM8960_AUDIO_BOARD_register_write_byte(uint8_t reg_addr, uint8_t data)
+static void wm8960_i2c_init()
 {
-    int ret;
-    uint8_t write_buf[2] = {reg_addr, data};
+    esp_err_t rc;
+    i2c_config_t i2c_conf;
+    memset(&i2c_conf, 0x00, sizeof(i2c_conf));
+    i2c_conf.mode = I2C_MODE_MASTER;
+    i2c_conf.sda_io_num = SDA_PIN;
+    i2c_conf.scl_io_num = SCL_PIN;
+    i2c_conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    i2c_conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    i2c_conf.master.clk_speed = 100 * 1000;
+    ESP_LOGI(TAG, "Configuring I2C");
+    rc = i2c_param_config(I2C_BUS_NO, &i2c_conf);
+    // ESP_LOGD(TAG, "I2C Param Config: %s", esp_err_to_name(rc));
+    rc = i2c_driver_install(I2C_BUS_NO, I2C_MODE_MASTER, 0, 0, 0);
+    // ESP_LOGD(TAG, "I2C Driver Install; %s", esp_err_to_name(rc));
+}
 
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, WM8960_AUDIO_BOARD_ADDR  , write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_RATE_MS);
+static esp_err_t i2c_trans(i2c_port_t i2c_num, uint8_t addr, void *txdata, uint8_t txlen)
+{
+    esp_err_t rc;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    // ESP_LOGV(TAG, "CMD Handle: %s", esp_err_to_name(rc));
+    rc = i2c_master_start(cmd);
+    // ESP_LOGV(TAG, "[W] Master_start: %s", esp_err_to_name(rc));
+    rc = i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    // ESP_LOGV(TAG, "[W] Master_write_byte: %s", esp_err_to_name(rc));
+    rc = i2c_master_write(cmd, txdata, txlen, ACK_CHECK_EN);
+    // ESP_LOGV(TAG, "[W] Master_write: %s", esp_err_to_name(rc));
+    rc = i2c_master_stop(cmd);
+    // ESP_LOGV(TAG, "[W] Master stop: %s", esp_err_to_name(rc));
+    rc = i2c_master_cmd_begin(I2C_BUS_NO, cmd, 10);
+    // ESP_LOGI(TAG, "[W] CMD begin: %s", esp_err_to_name(rc));
+    i2c_cmd_link_delete(cmd);
+    return rc;
+}
+
+static esp_err_t i2c_recv(i2c_port_t i2c_num, uint8_t addr, void *rxdata, uint8_t rxlen)
+{
+    esp_err_t rc;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (CODEC_ADDR << 1) | I2C_MASTER_READ, ACK_CHECK_DIS);
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (addr << 1), ACK_CHECK_EN);
+    if (rxlen > 1) {
+        i2c_master_read(cmd, rxdata, rxlen - 1, ACK_VAL);
+    }
+    i2c_master_read_byte(cmd, rxdata + rxlen - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+    rc = i2c_master_cmd_begin(i2c_num, cmd, 10);
+    printf("Recv: %d\n", rc);
+    i2c_cmd_link_delete(cmd);
+    return rc;
+}
+
+static esp_err_t write_register_i2c(uint8_t slave_id, uint32_t reg_addr, uint32_t reg_val)
+{
+    esp_err_t rc;
+    uint8_t buff[3];
+    buff[0] = (reg_addr << 1) | ((reg_val >> 8) & 0x0f);
+    buff[1] = reg_val & 0xff;
+    //printf("%x %x \t %x %x\n", reg_addr, reg_val, buff[0], buff[1]);
+    rc = i2c_trans(I2C_BUS_NO, slave_id, buff, 2);
+    return rc;
+}
+
+static esp_err_t read_register_i2c(uint8_t slave_id, uint8_t reg_addr)
+{
+    esp_err_t rc;
+    uint8_t data = 0;
+    rc = i2c_recv(I2C_BUS_NO, reg_addr, &data, 1);
+    printf("Read Value: %x\n", data);
+    return rc;
+
+    esp_err_t res;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+    res  = i2c_master_start(cmd);
+    res |= i2c_master_write_byte(cmd, (slave_id << 1) | I2C_MASTER_WRITE, 1 /*ACK_CHECK_EN*/);
+    res |= i2c_master_write_byte(cmd, (reg_addr << 1), 1 /*ACK_CHECK_EN*/);
+    res |= i2c_master_stop(cmd);
+    res |= i2c_master_cmd_begin(0, cmd, 1000 / portTICK_RATE_MS);
+    printf("%d\n", res);
+    i2c_cmd_link_delete(cmd);
+
+}
+
+esp_err_t wm8960_init()
+{
+    esp_err_t ret = 0;
+
+    wm8960_i2c_init();
+    for (int i = 0; i < sizeof(wm8960_reg_defaults)/sizeof(uint16_t); i += 2 ) {
+        printf("[%d] %x %x\n", i, wm8960_reg_defaults[i], wm8960_reg_defaults[i+1]);
+        ret = write_register_i2c(CODEC_ADDR, wm8960_reg_defaults[i], wm8960_reg_defaults[i+1]);
+        if (ret != 0) {
+            ESP_LOGE(TAG, "Error: %d", ret);
+            return ret;
+        }
+    }
+     if (ret == ESP_OK) {
+        ESP_LOGI(TAG, "WM8960 is up");
+    } else {
+        ESP_LOGE(TAG, "WM8960 setup error");
+    }
+    return ret;
+}
+esp_err_t wm8960_set_vol(int vol)
+{
+    esp_err_t ret = 0;
+    int vol_to_set = 0;
+    if (vol == 0) {
+        vol_to_set = 0;
+    } else {
+        volume = vol;
+        vol_to_set = (vol / 10) * 5 + 200;
+    }
+    ret |= write_register_i2c(CODEC_ADDR, 0xc, 0x100 | vol_to_set);
+    ret |= write_register_i2c(CODEC_ADDR, 0xb, 0x100 | vol_to_set);
 
     return ret;
 }
 
-
-static esp_err_t i2c_master_init(void)
+esp_err_t wm8960_set_mute(bool mute)
 {
-    int i2c_master_port = I2C_MASTER_NUM;
-
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SDA_IO,
-        .scl_io_num = I2C_MASTER_SCL_IO,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,
-    };
-
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    esp_err_t ret = 0;
+    if (mute) {
+        ret |= wm8960_set_vol(0);
+    } else {
+        ret |= wm8960_set_vol(volume);
+    }
+    return ret;
 }
+
+esp_err_t wm8960_get_volume(uint8_t* vol)
+{
+    *vol = volume;
+    return ESP_OK;
+}
+
 
 
 
 void app_main(void)
 {
 
-    ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C initialized successfully");
+    wm8960_init();
+    wm8960_set_vol(50);
 
+
+   
       /* Initialize NVS — it is used to store PHY calibration data */
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         err = nvs_flash_init();
     }
-    ESP_ERROR_CHECK(err);
-
-     /* Demonstrate writing by reseting the WM8960_AUDIO_BOARD */
-    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R0 , R0_DATA));
     
-    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R1 , R1_DATA));
-  
-    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R2 , R2_DATA));
-
-    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R3 , R3_DATA));
-
-    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R5 , R5_DATA));
- 
-    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R26 , R26_DATA));
- 
-    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R34 , R34_DATA));
-    //vTaskDelay(50 / portTICK_PERIOD_MS);
-    ESP_ERROR_CHECK(WM8960_AUDIO_BOARD_register_write_byte(R37 , R37_DATA));
-   
 
 
 
